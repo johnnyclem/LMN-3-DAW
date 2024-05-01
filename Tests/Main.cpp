2e516a9c837627dd248554f031089b9f62b99d7d
John Clem <johnnyclem@gmail.com>
2024-05-01 18:00:19 -0500

    remove tests folder from repo

--- Tests/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <juce_events/juce_events.h>
-
-int main(int argc, char **argv) {
-    juce::ScopedJuceInitialiser_GUI init;
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
